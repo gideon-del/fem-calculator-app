bf848ab323c019f8a693b3afee176a70bf275aa3
gideon-del <gideonchidi471@gmail.com>
2022-09-22 00:15:32 +0100

    Done with transitions

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
 "use strict";
 const themes = document.querySelectorAll(".toggle");
+const keys = document.querySelectorAll("[data-key]");
+const main = document.querySelector("[data-main]");
+const text = document.querySelector("[data-text]");
+const screen = document.querySelector("[data-screen]");
+const calcBg = document.querySelector("[data-calc]");
+const del = document.querySelector(".del");
+const reset = document.querySelector(".reset");
 themes.forEach((theme) => {
   theme.addEventListener("click", function (e) {
     if (e.target.classList.contains("active")) return;
     document.querySelector(".active").classList.remove("active");
     e.target.classList.add("active");
+    const themeNum = +e.target.dataset.value;
+    keys.forEach((key) => (key.dataset.key = `${themeNum}`));
+    main.dataset.main = `${themeNum}`;
+    text.dataset.text = `${themeNum}`;
+    screen.dataset.screen = `${themeNum}`;
+    calcBg.dataset.calc = `${themeNum}`;
+    del.dataset.del = `${themeNum}`;
+    reset.dataset.del = `${themeNum}`;
   });
 });
+console.log(del, reset);
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,24 @@ module.exports = {
         veryDarkCyan: "hsl(185, 58%, 25%)",
         orange: "hsl(25, 98%, 40%)",
         darkOrange: "hsl(25, 99%, 27%)",
+        lightGrayishYellow: "hsl(45, 7%, 89%)",
+        darkGrayishOrange: "hsl(35, 11%, 61%)",
+        // Text
+        veryDarkGrayishYellow: "hsl(60, 10%, 19%)",
+        //Theme 3
+        // Background
+        veryDarkVoiletMain: "hsl(268, 75%, 9%)",
+        veryDarkVoiletToggle: "hsl(268, 71%, 12%)",
+        // Key
+        darkVoilet: "hsl(281, 89%, 26%)",
+        vividMagenta: "hsl(285, 91%, 52%)",
+        pureCyan: "hsl(176, 100%, 44%)",
+        softCyan: "hsl(177, 92%, 70%)",
+        veryDarkVoilet: "hsl(268, 47%, 21%)",
+        darkMangenta: "hsl(290, 70%, 36%)",
+        // Text
+        lightYellow: "hsl(52, 100%, 62%)",
+        veryDarkBlue: "hsl(198, 20%, 13%)",
       },
       fontSize: {
         default: "32px",
